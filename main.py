e8b732f409e8878e6270286aa35007c0123befda
hokuseihal <hokuseihal@gmail.com>
2021-06-04 11:46:03 +0900

    add setting.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from multiprocessing import cpu_count
 
 import torch
-import torch.nn as nn
 from torchvision.utils import save_image
 
 import core
 from dataset import LinerCrackDataset as Dataset
+from setting import *
 from unet import UNet as Model
 from util import setcolor, miouf
-from loss import  FocalLoss,DiceLoss
 
 
 def operate(phase):
@@ -41,36 +40,27 @@ def operate(phase):
 
 
 if __name__ == '__main__':
-    import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--batchsize', type=int, default=8)
-    parser.add_argument('--epochs', type=int, default=500)
-    parser.add_argument('--savefolder', default='tmp')
-    parser.add_argument('--cpu', default=False, action='store_true')
-    parser.add_argument('--loss', default='CE')
-
-    args = parser.parse_args()
-    device = 'cuda' if (torch.cuda.is_available() and not args.cpu) else 'cpu'
+    device = 'cuda' if (torch.cuda.is_available() and not cpu) else 'cpu'
     # device='cpu'
     model = Model(out_channels=3).to(device)
     optimizer = torch.optim.Adam(model.parameters())
     clscolor = torch.tensor([[0, 0, 0], [255, 255, 255], [0, 255, 0]])
     criterion = nn.CrossEntropyLoss()
-    size = (256,256)
+    size = (256, 256)
     traindataset = Dataset(txt='datasets/liner/train.txt', size=size)
     valdataset = Dataset(txt='datasets/liner/val.txt', size=size)
-    traindataloader = torch.utils.data.DataLoader(traindataset, batch_size=args.batchsize, shuffle=True,
+    traindataloader = torch.utils.data.DataLoader(traindataset, batch_size=batchsize, shuffle=True,
                                                   num_workers=cpu_count())
-    valdataloader = torch.utils.data.DataLoader(valdataset, batch_size=args.batchsize, shuffle=True,
+    valdataloader = torch.utils.data.DataLoader(valdataset, batch_size=batchsize, shuffle=True,
                                                 num_workers=cpu_count())
     writer = {}
     startepoch = 0
-    savefolder = f'result/{args.savefolder}'
+    savefolder = f'result/{savefolder}'
     import os
 
     os.makedirs(savefolder, exist_ok=True)
-    for e in range(startepoch, args.epochs):
+    for e in range(startepoch, epochs):
         operate('train')
         operate('val')
         core.save(model=model, fol=savefolder, argdic=writer, dic=writer)
---
+++ setting.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+from loss import FocalLoss,DiceLoss
+
+size=(256,256)
+criterion=nn.CrossEntropyLoss()
+batchsize=8
+epochs=500
+savefolder='tmp'
+cpu=False
